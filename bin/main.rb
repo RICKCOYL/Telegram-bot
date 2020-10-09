1f8c2d46f91741428df3b5e474ecc41d9e9a3a61
RICKCOYL <rickoburu@gmail.com>
2020-10-09 14:09:30 +0300

    add random method picker for movie api

--- bin/main.rb
+++ bin/main.rb
@@ -17,11 +17,11 @@ require './lib/movies.rb'
             bot.api.send_message(chat_id: message.chat.id, text: 'Hello,i am Jarvis! do you want to see the random best movies then type /movies')
 
         when '/movie'
-            films = Movies.new
-            film = films.movie()
-           p film
+            film = Movies.new
+            films = film.movie()
+            filmz = films[rand(films.size)] 
             
-            bot.api.send_message(chat_id: message.chat.id, text: ("The name of the movie is #{film.first[:title]} and has a rank of #{film.first[:rank]}").to_s, date: message.date)
+            bot.api.send_message(chat_id: message.chat.id, text: ("The name of the movie is#{filmz[:imageUrl]} #{filmz[:title]} and has a rank of #{filmz[:rank]}").to_s, date: message.date)
         when '/stop'
 
             bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
